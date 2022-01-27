86456b5874a2fe92fa6fa5b575e2b1a4ce00d1da
Oliver Alt <oa@lacheweg21.de>
2022-01-27 21:23:39 +0100

    Adding new contracts for data streams

---
+++ src/MDD4All.SpecIF.DataProvider.Contracts/DataStreams/ISpecIfDataPublisher.cs
@@ -0,0 +1,10 @@
+﻿using MDD4All.SpecIF.DataModels;
+using System.Collections.Generic;
+
+namespace MDD4All.SpecIF.DataProvider.Contracts.DataStreams
+{
+    public interface ISpecIfDataPublisher
+    {
+        void PublishData(List<Resource> specIfdata);
+    }
+}
---
+++ src/MDD4All.SpecIF.DataProvider.Contracts/DataStreams/ISpecIfDataSubscriber.cs
@@ -0,0 +1,15 @@
+﻿using MDD4All.SpecIF.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDD4All.SpecIF.DataProvider.Contracts.DataStreams
+{
+
+    public delegate void SpecIfDataReceivedEventHandler(object eventSource, List<Resource> specIfData);
+
+    public interface ISpecIfDataSubscriber
+    {
+        event SpecIfDataReceivedEventHandler SpecIfDataReceived;
+    }
+}
