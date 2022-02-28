2ff6b97f6fec088d4036754430a1335b699a0c66
Oliver Alt <oa@lacheweg21.de>
2022-02-28 15:31:21 +0100

    New attributes for value converters

---
+++ src/MDD4All.SpecIF.DataProvider.Contracts/Attributes/SpecIfPropertyValuesAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MDD4All.SpecIF.DataProvider.Contracts.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class SpecIfPropertyValuesAttribute : Attribute
+    {
+        private string _propertyClassTitle = "";
+
+        public string PropertyClassID { get; set; } = "";
+        public string PropertyClassRevision { get; set; } = "";
+
+        public SpecIfPropertyValuesAttribute(string propertyClassTitle)
+        {
+            _propertyClassTitle = propertyClassTitle;
+        }
+    }
+}
---
+++ src/MDD4All.SpecIF.DataProvider.Contracts/Attributes/SpecIfResourceAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MDD4All.SpecIF.DataProvider.Contracts.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class SpecIfResourceAttribute : Attribute
+    {
+        private string _resourceClassTitle = "";
+
+        public string ResourceClassID { get; set; } = "";
+        public string ResourceClassRevision { get; set; } = "";
+
+        public SpecIfResourceAttribute(string resourceClassTitle)
+        {
+            _resourceClassTitle = resourceClassTitle;
+        }
+
+    }
+}
